1f55672a44e1da85160c05a110d68eac6f089255
Sherif Amr <sherif.amr.927@gmail.com>
2020-05-04 00:07:02 +0200

    Delete mail-test.js

--- playground/mail-test.js
+++
@@ -1,9 +0,0 @@
-const mailer = require('../utils/mail');
-
-const sendMail = async () => {
-  const url = mailer.createConfirmEmailLink('http://localhost:3000/users');
-  mailer.sendConfirmationEmail(url, 'demonionsphere@gmail.com', 'Sherif Amr');
-}
-
-sendMail();
-
